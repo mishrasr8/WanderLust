9df80aebf49fb3a4b1afb63635b4b536a1b34c8b
Suyash Mishra <165210736+mishrasr8@users.noreply.github.com>
2026-08-01 20:16:43 +0530

    Update default image URL in listing model

--- model/listing.js
+++ model/listing.js
@@ -13,7 +13,7 @@ const listingSchema=Schema({
     },
     image:{
         type:String,
-        default:"https://unsplash.com/photos/dramatic-sunset-over-a-dark-mountain-valley-j3f1lwXBuAI",
+        default:"https://images.unsplash.com/photo-1773176647951-d8f618dee942?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set:(v)=> v==="" ?"https://unsplash.com/photos/dramatic-sunset-over-a-dark-mountain-valley-j3f1lwXBuAI":v
     },
     price:{
