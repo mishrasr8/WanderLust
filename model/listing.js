58bbb85ef03bbeac34524ec140657040e62e2031
Suyash Mishra <suyashmishra4055@gmail.com>
2026-08-13 00:00:27 +0530

    Review and delete 2

--- model/listing.js
+++ model/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review=require("./review.js")
 
 const Schema = mongoose.Schema;
 
@@ -38,6 +39,13 @@ const listingSchema = new Schema({
         ref:"Review"
     }]
 });
+
+listingSchema.post("findOneAndDelete",async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id:{$in:listing.reviews}})
+    }
+})
+
 
 const Listing = mongoose.model("Listing", listingSchema);
 
