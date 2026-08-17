00ca1ce16c25754e755a3517f45027d3491769e8
Suyash Mishra <suyashmishra4055@gmail.com>
2026-08-17 23:44:49 +0530

    Li

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 const ExpressError=require("./utils/ExpressError.js");
 const wrapAsync=require("./utils/wrapAsync.js");
+const Listing=require("./model/listing.js");
 
 const listings=require("./routes/listing.js");
 const review=require("./routes/review.js");
