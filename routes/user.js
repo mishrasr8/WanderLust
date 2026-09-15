2b1afb24149ae2295d33542ed1364c6d58d9c223
Suyash Mishra <suyashmishra4055@gmail.com>
2026-09-15 11:14:52 +0530

    x

--- routes/user.js
+++ routes/user.js
@@ -7,14 +7,15 @@ const { saveRedirectUrl } = require("../middleware.js");
 
 const userController=require("../controllers/users.js");
 
-router.get("/signup",userController.renderSignup);
+router
+.route("signup")
+.get(userController.renderSignup)
+.post(userController.signup);
 
-
-router.post("/signup",userController.signup);
-
-router.get("/login",userController.renderLogin);
-
-router.post("/login",saveRedirectUrl,passport.authenticate('local',{failureRedirect: "/login",failureFlash: true}),userController.login);
+router
+.route("login")
+.get(userController.renderLogin)
+.post(saveRedirectUrl,passport.authenticate('local',{failureRedirect: "/login",failureFlash: true}),userController.login);
 
 router.get("/logout",userController.logout);
 
